75920315c32e96ae209ea8748507b83122a698ad
aarati_K <aarati_K@node.aarati-k-104638.advosuwmadison-pg0.wisc.cloudlab.us>
2021-09-01 15:21:44 -0500

    Remove output allocation checks

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@
 for s in {1,2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89}
 do
     cd data && python datagen.py ${s} && cd ..
-    taskset -c 39 ./canonical ${s}
+    taskset -c 38 ./canonical ${s}
     echo "---------------"
 done
