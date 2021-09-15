21b95f381c3570da1d947b59191ccb4f897dfcd3
aarati-K <aarati.agc10@gmail.com>
2021-09-15 07:56:33 -0500

    Modify run script - use less number of seeds

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-
-for s in {1,2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89}
+for s in {1,2,3,5,7,11,13,17,19,23}
 do
     cd data && python datagen.py ${s} && cd ..
-    taskset -c 38 ./canonical ${s}
+    taskset -c 39 ./canonical ${s}
     echo "---------------"
+    sleep 10
 done
